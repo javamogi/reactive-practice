84181c02485652b3665f0602218ba3dfe481470d
javamogi <javamogi@gmail.com>
2024-07-09 13:04:29 +0900

    fix: 회원 가입 코드 수정

--- src/main/java/com/reactivepractice/user/service/UserServiceImpl.java
+++ src/main/java/com/reactivepractice/user/service/UserServiceImpl.java
@@ -7,7 +7,6 @@ import com.reactivepractice.user.domain.UserRequest;
 import com.reactivepractice.user.service.port.UserRepository;
 import lombok.Builder;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.dao.DuplicateKeyException;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
@@ -16,17 +15,16 @@ import reactor.core.publisher.Mono;
 @Service
 @RequiredArgsConstructor
 @Builder
-@Slf4j
 public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
 
     @Override
     public Mono<UserResponse> register(UserRequest user){
-        return userRepository.save(User.from(user))
-                .map(UserResponse::of)
-                .onErrorResume(DuplicateKeyException.class,
-                        error -> Mono.error(new DuplicateKeyException("already exist email " + user.getEmail())));
+        return userRepository.findByEmail(user.getEmail())
+                .flatMap(u -> Mono.<User>error(new DuplicateKeyException("already exist email " + user.getEmail())))
+                .switchIfEmpty(Mono.defer(() -> userRepository.save(User.from(user))))
+                .map(UserResponse::of);
     }
 
     @Override
